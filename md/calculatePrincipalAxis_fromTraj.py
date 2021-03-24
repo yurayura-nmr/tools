22b0cfd0f558fde644e260fe985ff36d6c3c8888
yurayura-nmr <81198401+yurayura-nmr@users.noreply.github.com>
2021-03-24 21:49:34 +0900

    Update and rename 20210228_calculatePaxis_from_traj_newMDanalysisVersion.py to calculatePrincipalAxis_fromTraj.py

--- md/calculatePrincipalAxis_fromTraj.py
+++ md/calculatePrincipalAxis_fromTraj.py
@@ -1,24 +1,23 @@
+"""
+Still unsigned axis.
+Can easily break symmetry by defining N->C vector and taking cross-product
+of 1st principal axis with this vector, then requiring the vector directed
+so that this cross product be positive.
+"""
+
 import MDAnalysis
 from MDAnalysis.tests.datafiles import PSF, DCD
+
 import numpy as np
 
-
 # Your file must be read here
-#u = MDAnalysis.Universe("traj.pdb")
 ubiquitin = MDAnalysis.Universe("md_0_1_Esmall310.gro", "md_0_1_Esmall310.xtc")
-#ubiquitin = MDAnalysis.Universe("md_0_1_Esmall310.gro")
 
 for timeStep in ubiquitin.trajectory:
     print("Frame: {0:5d}, Time: {1:8.3f} ps".format(timeStep.frame, ubiquitin.trajectory.time))
-    #print("Rgyr: {0:g} A".format(ubiquitin.atoms.radius_of_gyration()))
 
     CA = ubiquitin.select_atoms("protein and name CA")
-    #print CA
-
     I = CA.moment_of_inertia()
-
-    #print I
-
     UT = CA.principal_axes()
 
     print UT[0]
