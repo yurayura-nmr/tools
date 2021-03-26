7d429268d46f81b1448b3b748c2eceb1aa2c8af1
yurayura-nmr <81198401+yurayura-nmr@users.noreply.github.com>
2021-03-26 13:05:26 +0900

    Update setup_new_pc_for_gromacs.sh

--- setup_new_pc_for_gromacs.sh
+++ setup_new_pc_for_gromacs.sh
@@ -5,46 +5,31 @@ Graduate School of Medicine
 
 https://github.com/yurayura-nmr/tools
 
-Setup of GROMACS on a new PC or a new Linux installation
+Setup of GROMACS on a new PC using Pop! OS
 A list of commands. Not to be run as a single script.
 """
 
 # 1. NVIDIA driver 
 """
 Hardest part is to properly install the NVIDIA driver.
-So it is best to get this out of the way first.
-
-BEST WAY IS TO USE A DISTRIBUTION THAT HAS NVIDIA DRIVER AND CUDA PREINSTALLED
-e.g. Pop! OS (use etcher to burn to USB. UNetboot thingy caused errors during install)
-
-Next time, let's try Pop! OS or another distro with NVIDIA drivers preinstalled.
-
-Easiest using the Mint GUI called Driver Manager.
-GUI > Driver Manager > Set to nvidia and hope it works. 
-If not, choose a different version of the driver or try manual install.
-390 works for most of my PCs, whereas 460 works only for some of them
+Luckily, this is easy in Pop! OS.
+Download image and use etcher to burn to USB. 
+(UNetboot thingy caused errors during install).
 """
-sudo apt purge *nvidia*                 # Need to go back first otherwise installs will fail.
-sudo apt-get install nvidia-driver-390  # 390 works best. Newer drivers cause problems on some cards.
-sudo reboot                             # Necessary
-nvidia-smi                              # Driver reccognizes GPU correctly? Even after reboot?
-
-# Or... (this sometimes but not often worked for me)
-sudo add-apt-repository ppa:graphics-drivers/ppa
-sudo apt-get update
-sudo apt-get install nvidia-driver-...
-
+nvidia-smi   # Driver reccognizes GPU correctly?
 
 
 # 2. Set static IP
 sudo apt-get install ssh
 """
-Also easiest via the Mint/Ubuntu GUI.
+Easiest via the 
 Go to network connection > properties > IPv4 > set IP
-IP:      choose
+IP:      10.249.63.XXX [e.g. fubuki: 127]
 Netmask: 255.255.255.0
-Gateway: 
-Reboot and make sure it worked. Sometimes it can disconnect everything.
+Gateway: 10.249.63.254 [router]
+
+Disconnect and reconnect, check if it works.
+e.g. if apt-get update still works.
 """
 
 # 3. CMAKE
@@ -69,56 +54,41 @@ sudo make install
 
 # 5. LAPACK (includes BLAS)
 # Get source
+sudo apt-get install gfortran
+
 wget https://github.com/Reference-LAPACK/lapack/archive/v3.9.0.tar.gz
 tar xvf v3.9.0.tar.gz
 mkdir testbuild
 cd testbuild
-sudo apt-get install gfortran
 cmake .. -DBUILD_SHARED_LIBS=ON # required to get libblas.so and liblapack.so
-make -j4
 
+make -j4
 sudo make install # note where the libraries are installed; this will be important later when GROMACS is compiled.
 
 # 6. CUDA
 """
 Get CUDA drivers from NVIDIA.
-
-* This also would be easier with Pop! OS, I guess...
-
-* Make sure that the CUDA version is compatible with whatever driver we get to work.
-  e.g. nvidia-390 supports CUDA 9.1 but not CUDA 11, etc.
-  
-* Moreover, the CUDA version must be compatible with the gcc version.
-  https://stackoverflow.com/questions/6622454/cuda-incompatible-with-my-gcc-version
-  
-* CUDA will sometimes complain that the driver is too old for current CUDA.
-  Newer drivers are great but sometimes they cause installation trouble.
-  As stated here, we may want to take the older driver and ignore the CUDA installer's
-  claim that CUDA won't work. In fact, it does work with older drivers.
-  https://www.pyimagesearch.com/2019/12/09/how-to-install-tensorflow-2-0-on-ubuntu/
+Again, Pop! OS comes to the rescue.
+https://support.system76.com/articles/cuda/
 """
-wget ...
-chmod +x run ...
-./run... -override # need to override otherwise, the installer always complains about the compiler
-
-# Pop! OS is easier!
-# https://support.system76.com/articles/cuda/
 sudo apt install system76-cuda-latest
+sudo apt-get install mlocate
+locate cuda # check where they went, we have to tell GROMACS this
 
 # 7. GROMACS part 1
 """
 Gromacs wants somewhat new, but CUDA wants somewhat old C and C++ compilers.
-Then we need to rewrite the host_config header so that the compile works.
+Sometimes we need to rewrite the host_config header so that the compile works.
+Not necessary for newer CUDA.
 """
 sudo apt-get install build-essential
 sudo apt-get install libhwloc-dev
 sudo apt-get install libomp-dev
-#sudo apt-get install openmpi-bin libopenmpi-dev
 sudo apt-get install gcc-7 g++-7
 sudo apt-get install vim
+#sudo apt-get install openmpi-bin libopenmpi-dev
 
-vi /usr/local/cuda-9.1/include/crt/host_config.h # change this file to work around it. GNU 7 compilers will just do fine.
-
+#vi /usr/local/cuda-9.1/include/crt/host_config.h # change this file to work around it. GNU 7 compilers will just do fine.
 # Comment out like this, i.e. leave everything as it is and just skip the error.
 #if __GNUC__ > 6
 //#error -- unsupported GNU version! gcc versions later than 6 are not supported!
@@ -129,10 +99,15 @@ vi /usr/local/cuda-9.1/include/crt/host_config.h # change this file to work arou
 Check output of cmake so we do not miss anything.
 was CUDA found? was fftw3 found? were blas and lapack found?
 """
+# get GROMACS source
+tar xvf ...
+mkdir testbuild
+cd testbuild
 cmake .. -DGMX_GPU=CUDA -DCMAKE_C_COMPILER=gcc-7 -DCMAKE_CXX_COMPILER=g++-7 -DREGRESSIONTEST_DOWNLOAD=ON -DGMX_BLAS_USER=/usr/local/lib/libblas.so -DGMX_LAPACK_USER=/usr/local/lib/liblapack.so -DGMX_OPENMP=ON -DGMX_MPI=OFF -DGMX_THREAD_MPI=ON -DCUDA_TOOLKIT_ROOT_DIR=/usr/lib/cuda-11.2
 
 make -j4
-make check -j4      # all tests must pass. Otherwise trouble with install or hardware. Can fall back on older gromacs.
+make check -j4      # all tests must pass. Otherwise trouble with install or hardware. Might be better to fall back on older gromacs if they work.
+
 ./bin/gmx --version # verify that CUDA was recognized during the build
 
 """
