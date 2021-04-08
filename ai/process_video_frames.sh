655f70f07ffe301dbf9a4531489afad96e6409fc
Erik Walinda <81198401+yurayura-nmr@users.noreply.github.com>
2021-04-08 16:20:11 +0900

    Update process_video_frames.sh

--- ai/process_video_frames.sh
+++ ai/process_video_frames.sh
@@ -4,11 +4,8 @@
 # Seems to require some amount of memory.
 
 for filename in *.png; do
-    for ((i=0; i<=3; i++)); do
-        cp $filename image.png
-        ./a.out
-        mv upscaled.png hq_$filename
-        echo "[ Done ] ", $filename
-        #./MyProgram.exe "$filename" "Logs/$(basename "$filename" .txt)_Log$i.txt"
-    done
+    cp $filename image.png
+    ./a.out
+    mv upscaled.png hq_$filename
+    echo "[ Done ] ", $filename   
 done
