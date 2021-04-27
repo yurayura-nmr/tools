9f86135e9e9dd2e9c12a47dc93bf7036546eef29
Erik Walinda <81198401+yurayura-nmr@users.noreply.github.com>
2021-04-27 15:20:38 +0900

    Update plot_paxis_tip_overtime.py
    Added python3 support. Tested with matplotlib 3.3.4.

--- md/plot_paxis_tip_overtime.py
+++ md/plot_paxis_tip_overtime.py
@@ -2,22 +2,25 @@
 
 """
 Erik Walinda
+
 Kyoto University
 Graduate School of Medicine
 
-Axes3D requires a weird version of matplotlib for the
-current script to work.
-The problem is in the coloring of the dots, i.e. the c=i statement.
+Last update: 2021/4/27
+* Rewrote c=object part of the ax.scatter
+* Thus, finally compatible with Python 3.8.5
+* Tested with matplotlib version 3.3.4
+
 """
-
 import matplotlib
+matplotlib.use('Agg')  # Force matplotlib to not use any Xwindows backend.
 
-# Force matplotlib to not use any Xwindows backend.
-matplotlib.use('Agg')
+print ("[ Matplotlib version ]", matplotlib.__version__)  # 3.3.4 works well
 
 import numpy
 import matplotlib.pyplot as plt
 
+from matplotlib import cm
 from mpl_toolkits.mplot3d import Axes3D
 
 def readFile(filename):
@@ -36,27 +39,33 @@ ax = fig.add_subplot(111, projection='3d')
 vectors = readFile("paxis_output.txt")
 
 """
-Start and end FRAME
+Specify start and end FRAMEs
+For example, a MD trajectory saved every 10 ps has 2000 frames to reach 20 ns in duration.
 
 2000 = 2000 * 10 ps = 20 ns
 """
 start = 0
 end   = 1000
-vectors=vectors[start:end]
 
+vectors = vectors[start:end]
 nColors = len(vectors)
 
-#cmap = plt.get_cmap('jet', len(vectors)*10)
-cmap = plt.get_cmap('jet', nColors)
+cmap = plt.get_cmap('jet', nColors)  # Can specify other colors by exchanging jet to any other matplotlib colormap
 cmap.set_under('gray')
 
-print (vectors)
+#print (vectors)
 
+"""
+Loop over the array (time boundaries specified by start and end) to draw 3D plot
+"""
 for i in range(nColors):
+    print("[\033[1;32m  Frame  \033[0m] ", i)
+
     vec = vectors[i]
-    print(i)
-    #cax = ax.scatter(vec[0], vec[1], vec[2], '-o', cmap=cmap, c=i, vmin=0, vmax=nColors, edgecolors='none')
-    cax = ax.scatter(vec[0], vec[1], vec[2], '-o', cmap=cmap, c=i,  vmin=0, vmax=nColors)
+
+    # Get color as integer value
+    color = [cmap(float(i) / nColors)]  # The use of [] here is a workaround since ax.scatter expects an array, not an integer.
+    cax = ax.scatter(vec[0], vec[1], vec[2], '-o', cmap=cmap, c=color, vmin=0, vmax=nColors, edgecolors='none')
 
 ax.set_xlim([-1, 1])
 ax.set_ylim([-1, 1])
@@ -66,9 +75,8 @@ ax.set_xlabel('x')
 ax.set_ylabel('y')
 ax.set_zlabel('z')
 
-#fig.colorbar(cax)
-
-#plt.show()
+fig.colorbar(cm.ScalarMappable(norm=None, cmap=cmap), ax=ax)
+#plt.show()  # Commented out for remote use via ssh.
 
 ax.view_init(elev=10, azim=7.5)
 plt.savefig("test_" + str(start) + "_" + str(end) + ".pdf")
