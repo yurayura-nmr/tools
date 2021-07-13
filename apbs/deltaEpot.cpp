e0a683588dace381d56b67b7f165a753603d90f3
Erik Walinda <81198401+yurayura-nmr@users.noreply.github.com>
2021-07-13 12:06:22 +0900

    Update deltaEpot.cpp

--- apbs/deltaEpot.cpp
+++ apbs/deltaEpot.cpp
@@ -9,10 +9,11 @@ Erik Walinda
 Kyoto University
 Graduate School of Medicine
 
-Read APBS 1.3 potential (kT/e) files and compute difference map.
+Read APBS (version 1 or 3) potential (kT/e) files and compute difference map.
 Manually remove header and footer lines and keep only numbers for processing.
 
 Example: ./deltaEpot pH5.dx pH6.dx
+i.e., if using: g++ deltaEpot.cpp -o deltaEpot
 Suggestion: avoid confusion by putting lower pH first always.
 
 */
@@ -20,20 +21,19 @@ Suggestion: avoid confusion by putting lower pH first always.
 using namespace std;
 
 int process (char *filename_1, char *filename_2) {
-    // Open the gromacs-created xvg file. [first pH, second pH] 
+    // Open the input and output files. [first pH, second pH] 
     ifstream in1(filename_1);
     ifstream in2(filename_2);
     ofstream out("difference.dx");
+	
     out << "(add header and footer lines before viewing in pymol)" << endl;
-    out << "(d1" << right << setw(12) << "d2" << right << setw(12) << "d3)" << right << setw(12) << endl;
+    //out << "(d1" << right << setw(12) << "d2" << right << setw(12) << "d3)" << right << setw(12) << endl;
 
-    // We want to read time t and distance x(t) from gromacs output
-    // For each data point we need three numbers.
+    // Read APBS data for both files for each point. Each contains three numbers.
     double in1_a1, in1_a2, in1_a3;
     double in2_a1, in2_a2, in2_a3;
    
-    // Count the number of lines containing data 
-    int timeStep;
+    int timeStep; // Count the number of lines containing data 
     timeStep = 0;
     double diff_a1, diff_a2, diff_a3;
 
@@ -57,14 +57,13 @@ int process (char *filename_1, char *filename_2) {
                     //cout << "[Read line 2nd pH ]: " << "# " << timeStep << " a1: " << in2_a1 << " a2: " << in2_a2 << " a3: " << in2_a3 << endl;
                     //cout << "[ Evaluating line ]: " << "# " << timeStep << " a1: " << diff_a1 << " a2: " << diff_a2 << " a3: " << diff_a3 << endl;
                     out << diff_a1 << setw(12) << diff_a2 << setw(12) << diff_a3 << setw(12) << endl;
-
                     timeStep++;
-
                 }
 	    }
 
     in1.close();
     in2.close();
+    out.close();
     return 0;
 }
 
