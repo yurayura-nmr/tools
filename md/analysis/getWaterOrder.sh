d60cdb19332170e5d3e11b85062a9940e76738a1
Erik Walinda <81198401+yurayura-nmr@users.noreply.github.com>
2021-07-14 16:10:07 +0900

    Update getWaterOrder.sh

--- md/analysis/getWaterOrder.sh
+++ md/analysis/getWaterOrder.sh
@@ -12,3 +12,14 @@ Report average for each slice.
 gmx h2order -f md_0_1_E10000M.xtc -s md_0_1_E10000M.tpr -n index.ndx -d X -o orderX.xvg
 gmx h2order -f md_0_1_E10000M.xtc -s md_0_1_E10000M.tpr -n index.ndx -d Y -o orderY.xvg
 gmx h2order -f md_0_1_E10000M.xtc -s md_0_1_E10000M.tpr -n index.ndx -d Z -o orderZ.xvg
+
+# Test if any periodic behaviour occurs
+
+gmx h2order -f md_0_1_E10000M.xtc -s md_0_1_E10000M.tpr -n index.ndx -d X -o orderX_00.xvg -b 0 -e 100
+gmx h2order -f md_0_1_E10000M.xtc -s md_0_1_E10000M.tpr -n index.ndx -d Y -o orderY_00.xvg -b 0 -e 100
+gmx h2order -f md_0_1_E10000M.xtc -s md_0_1_E10000M.tpr -n index.ndx -d Z -o orderZ_00.xvg -b 0 -e 100
+
+gmx h2order -f md_0_1_E10000M.xtc -s md_0_1_E10000M.tpr -n index.ndx -d X -o orderX_01.xvg -b 100 -e 200
+gmx h2order -f md_0_1_E10000M.xtc -s md_0_1_E10000M.tpr -n index.ndx -d Y -o orderY_01.xvg -b 100 -e 200
+gmx h2order -f md_0_1_E10000M.xtc -s md_0_1_E10000M.tpr -n index.ndx -d Z -o orderZ_01.xvg -b 100 -e 200
+
